2ed40ae1aad26ee77b0631f3d45dcfb7334aed34
Rasim Alagezov <r.alagezov@gmail.com>
2023-01-15 22:04:50 +0400

    End of Section 15

---
+++ TangyWeb_Client/Service/IService/ICartService.cs
@@ -0,0 +1,11 @@
+﻿using TangyWeb_Client.ViewModels;
+
+namespace TangyWeb_Client.Service.IService
+{
+	public interface ICartService
+	{
+        public event Action OnChange;
+        Task DecrementCart(ShoppingCart cartToDecrement);
+		Task IncrementCart(ShoppingCart cartToAdd);
+	}
+}
