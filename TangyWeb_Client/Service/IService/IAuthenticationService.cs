7ea0253f4acaff0aae3d7f406e02982b4b8d789c
Rasim Alagezov <r.alagezov@gmail.com>
2023-01-28 13:15:14 +0400

    End of Section 19

---
+++ TangyWeb_Client/Service/IService/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using Tangy_Models.DTOs;
+
+namespace TangyWeb_Client.Service.IService
+{
+    public interface IAuthenticationService
+    {
+        Task<SignUpResponseDTO> RegisterUser(SignUpRequestDTO signUpRequestDTO);
+        Task<SignInResponseDTO> Login(SignInRequestDTO signInRequestDTO);
+        Task Logout();
+    }
+}
---
+++ Tangy_Models/DTOs/SignInRequestDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models.DTOs
+{
+    public class SignInRequestDTO
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
